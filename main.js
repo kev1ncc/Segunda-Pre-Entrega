18b2d72f8ff22ec610e1e14c36edf17386423188
kevincaballero95@hotmail.com <kevincaballero95@hotmail.com>
2023-02-15 19:20:36 -0300

    segunda-pre-entrega

---
+++ main.js
@@ -0,0 +1,99 @@
+const carrito = []
+
+
+const ordenarMenorMayor = () => {
+    productos.sort((a, b) => a.precio - b.precio)
+    mostrarListaOrdenada()
+};
+
+const ordenarMayorMenor = () => {
+    productos.sort((a, b) => b.precio - a.precio)
+    mostrarListaOrdenada()
+};
+
+const mostrarListaOrdenada = () => {
+    const listaDeProductos = productos.map(producto => {
+        return '- '+producto.nombre+' $'+producto.precio
+    })
+    alert('Lista de precios:'+'\n\n'+listaDeProductos.join('\n'))
+    comprarProductos(listaDeProductos)
+};
+
+const comprarProductos = (listaDeProductos) => {
+    let productoNombre = ''
+    let productoCantidad = 0
+    let otroProducto = false
+
+    do {
+        productoNombre = prompt('¿Qué producto desea comprar?'+'\n\n'+listaDeProductos.join('\n'))
+        productoCantidad = parseInt(prompt('¿Cuántos queres comprar?'))
+
+        const producto = productos.find(producto => producto.nombre.toLowerCase() === productoNombre.toLowerCase())
+
+        if (producto) {
+            agregarAlCarrito(producto, producto.id, productoCantidad)
+        } else {
+            alert('El producto no se encuentra en el catálogo!')
+        }
+
+        otroProducto = confirm('Desea agregar otro producto?')
+    } while (otroProducto);
+
+    confirmarCompra()
+};
+
+
+const agregarAlCarrito = (producto, productoId, productoCantidad) => {
+    const productoRepetido = carrito.find(producto => producto.id === productoId)
+    if (!productoRepetido) {
+        producto.cantidad += productoCantidad
+        carrito.push(producto)
+    } else {
+        productoRepetido.cantidad += productoCantidad
+    }
+};
+
+
+const confirmarCompra = () => {
+    const listaProductos = carrito.map(producto => {
+        return '- '+producto.nombre+' | Cantidad: '+producto.cantidad
+    })
+
+    const isCheckout = confirm('Checkout: '
+        +'\n\n'+listaProductos.join('\n')
+        +'\n\nPara continuar presione "Aceptar" sino "Cancelar" para seguir comprando'
+    )
+
+    if (isCheckout) {
+        finalizarCompra(listaProductos)
+    } else {
+        ordenarMenorMayor()
+    }
+};
+
+
+
+const finalizarCompra = (listaProductos) => {
+    const cantidadTotal = carrito.reduce((acc, item) => acc + item.cantidad, 0)
+    const precioTotal = carrito.reduce((acc, item) => acc + (item.cantidad * item.precio), 0)
+    alert('Detalle de su compra: '
+        +'\n\n'+listaProductos.join('\n')
+        +'\n\nTotal de productos: '+cantidadTotal
+        +'\n\nEl total de su compra es: '+precioTotal
+        +'\n\nGracias por su compra!'
+    )
+};
+
+const comprar = () => {
+    const productosBaratos = confirm('¿Querés ordenar la lista de productos del mas barato al mas caro?')
+
+    if (productosBaratos) {
+        ordenarMenorMayor()
+    } else {
+        ordenarMayorMenor()
+    }
+};
+
+
+comprar()
+
---
+++ stock.js
@@ -0,0 +1,58 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Asado",
+        tipo: "Asado",
+        descripcion: "Es el corte emblemático de la parrilla argentina. El contenido de grasa le da un sabor incomparable.",
+        precio: 1350,
+        cantidad:0
+    },
+
+    {
+        id: 2,
+        nombre: "Suprema",
+        tipo: "Suprema",
+        descripcion: "La pechuga de pollo es, por su gran aprovechamiento y bajo contenido graso, utilizado para cocinar una gran variedad de platos.",
+        precio: 1450,
+        cantidad:0
+    },
+
+    {
+        id: 3,
+        nombre: "Bondiola",
+        tipo: "Bondiola",
+        descripcion: "Corte popular; la preparación favorita es a la parrilla. Su terneza y gusto lo hacen muy apto para su consumo en sándwich.",
+        precio: 1900,
+        cantidad:0
+    },
+
+
+    {
+        id: 4,
+        nombre: "Bola de lomo",
+        tipo: "Bola de lomo",
+        descripcion: "Se caracteriza por ser un corte tierno y económico, su uso mas popular es para milanesas.",
+        precio: 1000,
+        cantidad:0
+    },
+
+    {
+        id: 5,
+        nombre: "Pata y muslo",
+        tipo: "Pata y muslo",
+        descripcion: "Se cocinan a la plancha o en horno. Su piel, grasa y huesos le dan un gran sabor. El peso aproximado de la pieza (1 pata con 1 muslo) es de 0,600 kg.",
+        precio: 490,
+        cantidad:0
+    },
+
+    {
+        id: 6,
+        nombre: "Fife ancho",
+        tipo: "Fife ancho",
+        descripcion: "Para preparar a la parrilla o plancha, la presencia del hueso en su cocción le da un gusto especial.",
+        precio: 1000,
+        cantidad:0
+    },
+
+]
